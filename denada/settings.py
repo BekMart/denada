2202c9eeebd25778fdc32371e19af2f37d95c0da
Rebecca Martin <becki_213@hotmail.com>
2024-10-01 14:29:33 +0000

    amend settings for deployment

--- denada/settings.py
+++ denada/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5qh37v#d3p$vudsht+mmre!=o2m^-+pb^!!3yu@5$3uovfq0+6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-bekmart-denada-ufzzin8nbc5.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-bekmart-denada-ufzzin8nbc5.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
