38e95c4de847250ddd9611087c52b9977191c502
Rebecca Martin <becki_213@hotmail.com>
2024-10-02 10:41:37 +0000

    create menu.html and link to base.html template

--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def menu_page(request):
-    return HttpResponse("TAKE A LOOK AT OUR MENUS!")
+    print("About to render template")
+
+    return render(
+        request,
+        "menu/menu.html",
+    )
