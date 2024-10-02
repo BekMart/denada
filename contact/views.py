1228f268f9b980a275578bf4a4c3ce61c1f3c13c
Rebecca Martin <becki_213@hotmail.com>
2024-10-02 10:50:03 +0000

    create contact.html and link to base.html template

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def contact_page(request):
-    return HttpResponse("GET IN TOUCH!")
+    print("About to render template")
+
+    return render(
+        request,
+        "contact/contact.html",
+    )
