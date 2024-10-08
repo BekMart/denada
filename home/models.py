a228c25ecb46e7b674f3424eb24202d8be966119
Rebecca Martin <becki_213@hotmail.com>
2024-10-08 08:45:32 +0000

    move restaurant model to home and migrate all models in home and menu

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    about = models.TextField()
+    address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=13)
+    email = models.EmailField()
+    max_capacity = models.IntegerField()
+    num_tables = models.IntegerField()
+    restaurant_image = CloudinaryField('image', default='placeholder', blank=True)
--- menu/models.py
+++ menu/models.py
@@ -1,19 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from home.models import Restaurant
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
-class Restaurant(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    description = models.TextField()
-    address = models.CharField(max_length=255)
-    phone = models.CharField(max_length=13)
-    email = models.EmailField()
-    max_capacity = models.IntegerField()
-    num_tables = models.IntegerField()
-    restaurant_image = CloudinaryField('image', default='placeholder', blank=True)
-
 class Type(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
