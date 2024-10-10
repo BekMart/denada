3f8c0a3b1643dc68b6babd8a082f4ba143b5374b
Rebecca Martin <becki_213@hotmail.com>
2024-10-10 13:38:16 +0000

    add authentication and ammend login and logout pages

--- denada/settings.py
+++ denada/settings.py
@@ -52,9 +52,7 @@ INSTALLED_APPS = [
     'home',
     'menu',
     'book',
-    'contact',
-    'login',
-    'signup',
+    'contact'
 ]
 
 
--- denada/urls.py
+++ denada/urls.py
@@ -22,9 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('book/', include('book.urls'), name='book-urls'),
     path('contact/', include('contact.urls'), name='contact-urls'),
-    path('login/', include('login.urls'), name='login-urls'),
     path('menu/', include('menu.urls'), name='menu-urls'),
-    path('signup/', include('signup.urls'), name='signup-urls'),
     path('summernote/', include('django_summernote.urls')),
     path('', include('home.urls'), name='home-urls'),
 ]
