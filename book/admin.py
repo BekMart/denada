64d64dbffde0e51cdff0b36f2b17ecbd63c34d5a
Rebecca Martin <becki_213@hotmail.com>
2024-10-14 14:59:05 +0000

    create booking model and add to admin panel. install crsipy forms ready to create booking form

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Booking, STATUS
 
-# Register your models here.
+# # Register your models here.
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+
+    list_display = ('user', 'restaurant', 'party_size', 'date', 'time', 'status')
+    search_fields = ['user', 'restaurant']
+    list_filter = ('restaurant', 'date', 'time', 'status')
+
+    def get_status_display(self, obj):
+        return STATUS[obj.status][1]  # Access the second element (string value)
--- book/models.py
+++ book/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from home.models import Restaurant
+
+STATUS = ((0, "Pending"), (1, "Confirmed"), (2, "Declined"))
 
 # Create your models here.
+class Booking(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_booking"
+    )
+    restaurant = models.ForeignKey(
+        Restaurant, on_delete=models.CASCADE, related_name="restaurant_booking"
+    )
+    party_size = models.IntegerField()
+    date = models.DateField()
+    time = models.TimeField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    special_requests = models.TextField(blank=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"Booking for {self.restaurant} | {self.party_size} guests | {self.date} | {self.time} | {self.status}"
