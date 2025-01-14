b0a099db33ba6a2f17683eb22ccc42f92f3b9ba5
Rebecca Martin <becki_213@hotmail.com>
2025-01-14 16:27:49 +0000

    Add models for content on home page and register them to admin panel

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Restaurant
+from .models import Restaurant, About, Produce, Visit
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Restaurant)
@@ -10,5 +10,23 @@ class RestaurantAdmin(SummernoteModelAdmin):
     list_filter = ('name',)
     prepopulated_fields = {'slug': ('name',)}
     summernote_fields = ('about',)
+
+@admin.register(About)
+class AboutAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'id',)
+    summernote_fields = ('content',)
+
+@admin.register(Produce)
+class ProduceAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'id',)
+    summernote_fields = ('content',)
+
+@admin.register(Visit)
+class VisitAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'id',)
+    summernote_fields = ('content',)
 
 # Register your models here.
--- home/models.py
+++ home/models.py
@@ -15,3 +15,36 @@ class Restaurant(models.Model):
 
     def __str__(self):
         return self.name
+
+class About(models.Model):
+    restaurant = models.ForeignKey(
+        Restaurant, on_delete=models.CASCADE, related_name="restaurant_about", default=1
+    )
+    title = models.CharField(max_length=50, unique=True)
+    content = models.TextField()
+    updated_on = models.DateTimeField(auto_now_add=True)
+    image_1 = CloudinaryField('image', default='placeholder', blank=True)
+    image_2 = CloudinaryField('image', default='placeholder', blank=True)
+    image_3 = CloudinaryField('image', default='placeholder', blank=True)
+
+class Produce(models.Model):
+    restaurant = models.ForeignKey(
+        Restaurant, on_delete=models.CASCADE, related_name="restaurant_produce", default=1
+    )
+    title = models.CharField(max_length=50, unique=True)
+    content = models.TextField()
+    updated_on = models.DateTimeField(auto_now_add=True)
+    image_1 = CloudinaryField('image', default='placeholder', blank=True)
+    image_2 = CloudinaryField('image', default='placeholder', blank=True)
+    image_3 = CloudinaryField('image', default='placeholder', blank=True)
+
+class Visit(models.Model):
+    restaurant = models.ForeignKey(
+        Restaurant, on_delete=models.CASCADE, related_name="restaurant_visit", default=1
+    )
+    title = models.CharField(max_length=50, unique=True)
+    content = models.TextField()
+    updated_on = models.DateTimeField(auto_now_add=True)
+    image_1 = CloudinaryField('image', default='placeholder', blank=True)
+    image_2 = CloudinaryField('image', default='placeholder', blank=True)
+    image_3 = CloudinaryField('image', default='placeholder', blank=True)
