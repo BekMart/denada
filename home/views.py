4c8539a4d985965404dc34f37a5fb0ea8d3be4a9
Rebecca Martin <becki_213@hotmail.com>
2025-01-15 16:21:40 +0000

    Input data into new models and upload images via cloudinary and static files. Upldate view and template files to display data from database

--- home/views.py
+++ home/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+from .models import Restaurant, About, Produce, Visit
 
 # Create your views here.
 def homepage(request):
     print("About to render template")
 
+    latest_about = About.objects.latest('updated_on') 
+    latest_produce = Produce.objects.latest('updated_on')
+    latest_visit = Visit.objects.latest('updated_on')
+
+    context = {
+        'about': latest_about,
+        'produce': latest_produce,
+        'visit': latest_visit,
+    }
+
     return render(
         request,
         "home/index.html",
+        context
     )
