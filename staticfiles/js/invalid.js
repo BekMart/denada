17f0f3581d92d87a6fb50056245baba356b17b82
Rebecca Martin <becki_213@hotmail.com>
2025-01-29 11:45:21 +0000

    Remove invalid feedback from edit_booking form using css and create invalid.js file to remove is-invalid class to remove red icon

---
+++ staticfiles/js/invalid.js
@@ -0,0 +1,7 @@
+const partyInput = document.getElementById('id_party_size');
+const dateInput = document.getElementById('id_date');
+const timeInput = document.getElementById('id_time');
+
+partyInput.classList.remove('is-invalid'); 
+dateInput.classList.remove('is-invalid'); 
+timeInput.classList.remove('is-invalid'); 
