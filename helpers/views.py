35c2c31fea00a310657eb0f4c445865840d55cb8
BekMart <bekmart1989@gmail.com>
2025-02-06 20:22:05 +0000

    Update helpers/views.py file with docstring

--- helpers/views.py
+++ helpers/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
-def handle_error_404(request, exception):
-    return render(request, '404.html')
+
+def handle_error_404(request):
+    """
+    Handles 404 Page Not Found errors.
+    This view is triggered when a requested page is not found for better user experience.
+    - Takes the incoming request object.
+    - Returns the rendered 404 error page.
+    """
+    return render(
+        request,
+        '404.html'
+    )
