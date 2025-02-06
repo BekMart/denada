43185447a1133bb8cc9297d7ea1360391d1ba510
BekMart <bekmart1989@gmail.com>
2025-02-06 18:14:04 +0000

    Update book/urls.py file with comments

--- book/urls.py
+++ book/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+# Define URL patterns for the booking app
 urlpatterns = [
-    path('', views.booking_page, name='book'),
-    path('edit_booking/<int:booking_id>/', views.edit_booking, name='edit_booking'),
-    path('cancel_booking/<int:booking_id>/', views.cancel_booking, name='cancel_booking'),
+    path('', views.booking_page, name='book'),  # Route for booking page
+    path('edit_booking/<int:booking_id>/', views.edit_booking, name='edit_booking'),  # Route for editing an existing booking
+    path('cancel_booking/<int:booking_id>/', views.cancel_booking, name='cancel_booking'),  # Route for canceling a booking
 ]
