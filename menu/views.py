ef91fab262255dc721ab93e08b9968d74b14b89e
BekMart <bekmart1989@gmail.com>
2025-02-06 22:39:36 +0000

    Update menu/views.py file with comments and docstrings

--- menu/views.py
+++ menu/views.py
@@ -1,31 +1,48 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render
 from django.views import generic
-from .models import Menu, Type, Food, Drink
-from home.models import Restaurant
+from .models import Menu, Food, Drink
 
-# Create your views here.
+
 def menu_page(request):
-    print("About to render template")
-
+    """
+    Renders the menu page with the latest menu details.
+    - Takes the incoming request object.
+    - Renturns the most recently updated records of the menu details 
+    and passes it to the template.
+    """
+    # Retrieve the latest updated menu details
     latest_menu = Menu.objects.latest('updated_on')
 
+    # Context dictionary to pass data to the template
     context = {
         'menu': latest_menu,
     }
 
+    # Render and return the menu page with the latest menu details
     return render(
         request,
         "menu/menu.html",
         context
     )
 
+
 class FoodList(generic.ListView):
-    queryset = Food.objects.all().order_by("type")
-    template_name = "menu/food.html"
-    context_object_name = 'food_list'
+    """
+    Displays a list of Food items.
+    - Retrieves all Food objects ordered by their type.
+    - Renders the context_object_name to the template_name address.
+    """
+    queryset = Food.objects.all().order_by("type")  # Order food by type name instead of type ID
+    template_name = "menu/food.html"  # The file path of where to display list
+    context_object_name = 'food_list'  # List to be sent
 
 
 class DrinkList(generic.ListView):
-    queryset = Drink.objects.all().order_by("type")
-    template_name = "menu/drink.html"
-    context_object_name = 'drink_list'
+    """
+    Displays a list of Drink items.
+    - Retrieves all Food objects ordered by their type.
+    - Renders the context_object_name to the template_name address.
+    """
+    queryset = Drink.objects.all().order_by("type")  # Order drinks by type name instead of type ID
+    template_name = "menu/drink.html"  # The file path of where to display list
+    context_object_name = 'drink_list'  # List to be sent
