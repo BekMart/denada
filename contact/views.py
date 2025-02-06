1c4249d85e928e3a7e63db66bd933683adad01cc
BekMart <bekmart1989@gmail.com>
2025-02-06 20:16:04 +0000

    Update contact/views.py file with comments and docstrings

--- contact/views.py
+++ contact/views.py
@@ -2,16 +2,23 @@ from django.shortcuts import render
 from home.models import Restaurant
 from .models import Time, Address, Contact, Social
 
-# Create your views here.
-def contact_page(request):
-    print("About to render template")
 
+def contact_page(request):
+    """
+    Renders the contact page with the latest restaurant details.
+    - Takes the incoming request object.
+    - Fetches the most recently updated records for the restaurant's details.
+    - Including business hours, address, contact information, and social media links.
+    """
+
+    # Retrieve the latest updated details for the restaurant
     latest_details = Restaurant.objects.latest('updated_on')
     latest_time = Time.objects.latest('updated_on')
     latest_address = Address.objects.latest('updated_on')
     latest_contact = Contact.objects.latest('updated_on')
     latest_social = Social.objects.latest('updated_on')
 
+    # Context dictionary to pass data to the template
     context = {
         'details': latest_details,
         'time': latest_time,
@@ -20,6 +27,7 @@ def contact_page(request):
         'social': latest_social,
     }
 
+    # Render and return the contact page with the latest details
     return render(
         request,
         "contact/contact.html",
