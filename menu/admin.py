c0ddd3b8e4cfa4c2c4c78defa115a5acc1c0307f
BekMart <bekmart1989@gmail.com>
2025-02-06 22:12:16 +0000

    Update menu/admin.py file with comments and docstrings

--- menu/admin.py
+++ menu/admin.py
@@ -2,37 +2,53 @@ from django.contrib import admin
 from .models import Menu, Type, Food, Drink
 from django_summernote.admin import SummernoteModelAdmin
 
-@admin.register(Menu)
-class TypeAdmin(SummernoteModelAdmin):
+"""
+Admin configuration for the Menu app.
+This module registers and customizes the administration interface
+for the Menu, Type, Food, and Drink models using Summernote for rich-text fields.
+"""
 
-    list_display = ('restaurant', 'id',)
-    summernote_fields = ('about', 'note')
+
+@admin.register(Menu)
+class MenuAdmin(SummernoteModelAdmin):
+    """
+    Admin interface for the Menu model.
+    """
+    list_display = ('restaurant', 'title',)
+    summernote_fields = ('about', 'note')  # Enables Summernote editor for these fields
+
 
 @admin.register(Type)
 class TypeAdmin(SummernoteModelAdmin):
+    """
+    Admin interface for the Type model.
+    """
+    list_display = ('name',)
+    search_fields = ['name']  # Enables search functionality by type name
+    list_filter = ('name',)  # Adds a filter by name
+    prepopulated_fields = {'slug': ('name',)}  # Auto-generates slug from the name
+    summernote_fields = ('description')  # Enables Summernote editor for description field
 
-    list_display = ('name', 'id',)
-    search_fields = ['name',]
-    list_filter = ('name',)
-    prepopulated_fields = {'slug': ('name',)}
-    summernote_fields = ('description',)
 
 @admin.register(Food)
 class FoodAdmin(SummernoteModelAdmin):
-
+    """
+    Admin interface for the Food model.
+    """
     list_display = ('name', 'type', 'price', 'calories',)
-    search_fields = ['name', 'type',]
+    search_fields = ['name', 'type__name']
     list_filter = ('type', 'vegetarian', 'vegan', 'gluten',)
     prepopulated_fields = {'slug': ('name',)}
-    summernote_fields = ('description',)
+    summernote_fields = ('description')
+
 
 @admin.register(Drink)
 class DrinkAdmin(SummernoteModelAdmin):
-
+    """
+    Admin interface for the Drink model.
+    """
     list_display = ('name', 'type', 'price', 'calories',)
-    search_fields = ['name', 'type',]
+    search_fields = ['name', 'type__name']
     list_filter = ('type', 'vegetarian', 'vegan', 'gluten',)
     prepopulated_fields = {'slug': ('name',)}
-    summernote_fields = ('description',)
-
-# Register your models here.
+    summernote_fields = ('description')
